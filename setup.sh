c5166dd91949ecd9096ec63737db8358e3f98060
kvracop <jardelsousa.jlsilva@gmail.com>
2024-08-13 19:58:43 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-apt update
-apt upgrade
-URL=https://www.aapanel.com/script/install_7.0_en.sh && if [ -f /usr/bin/curl ];then curl -ksSO "$URL" ;else wget --no-check-certificate -O install_7.0_en.sh "$URL";fi; yes | bash install_7.0_en.sh 93684c35 > installTest.log 2>&1
+# Atualiza e atualiza o sistema
+apt-get update -y
+apt-get upgrade -y
+
+# Baixa e instala o aaPanel
+URL=https://www.aapanel.com/script/install_7.0_en.sh
+if [ -f /usr/bin/curl ]; then
+    curl -ksSO "$URL"
+else
+    wget --no-check-certificate -O install_7.0_en.sh "$URL"
+fi
+yes | bash install_7.0_en.sh 93684c35 > installTest.log 2>&1
+
+# Baixa e instala o Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
-apt install docker-compose-plugin
+bash get-docker.sh
+
+# Instala o plugin docker-compose
+apt-get install -y docker-compose-plugin
