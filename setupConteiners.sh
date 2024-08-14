faa86ce05cb45004e27ae2feac72c0396e2d9fa9
kvracop <jardelsousa.jlsilva@gmail.com>
2024-08-14 18:12:26 -0300

    Update setupConteiners.sh

--- setupConteiners.sh
+++ setupConteiners.sh
@@ -2,6 +2,7 @@
 docker run -d --name myjenkins --restart always -p 8080:8080 -v /var/jenkins_home jenkins/jenkins:lts
 docker run -d --name n8n --restart always -p 5678:5678 -v /var/n8n_home:/home/node/.n8n docker.n8n.io/n8nio/n8n
 docker run -d --name mariadb-glpi --restart always -e MYSQL_DATABASE=glpi -e MYSQL_USER=glpi -e MYSQL_PASSWORD=glpi -e MYSQL_RANDOM_ROOT_PASSWORD=1 -p 3306:3306 fametec/glpi:mariadb
+docker run -d --name glpi --link mariadb-glpi:mariadb-glpi -e GLPI_LANG=pt_BR -e MARIADB_HOST=mariadb-glpi -e MARIADB_PORT=3306 -e MARIADB_DATABASE=glpi -e MARIADB_USER=glpi -e MARIADB_PASSWORD=glpi -e VERSION="9.5.6" -e PLUGINS="all" -p 22571:80 fametec/glpi:latest
 mkdir -p /srv/gitlab
 chmod 777 /srv/gitlab
 echo "export GITLAB_HOME=/srv/gitlab" > ~/.bashrc
